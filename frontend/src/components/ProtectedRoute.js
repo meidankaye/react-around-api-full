d8da3d9842f12d86ec470ce68f0dc33b372f5055
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-06-29 18:50:05 +0300

    feat: no frontend?

--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ loggedIn, children, ...props  }) => {
-  return loggedIn ? children : <Navigate {...props} to="/signin" />
-}
-
-export default ProtectedRoute;
