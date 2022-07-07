d5db279af6ab3e9b35c72703d34b23cbf70820ed
meidankaye <87809871+meidankaye@users.noreply.github.com>
2022-07-07 15:11:23 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,20 +5,12 @@ const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 
-mongoose.connect('mongodb://localhost:27017/aroundb');
+mongoose.connect('mongodb://0.0.0.0:27017/aroundb');
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6290e407637e876c7bb47c34',
-  };
-
-  next();
-});
-
-app.use(usersRouter);
-app.use(cardsRouter);
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
 
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Requested resource not found' });
